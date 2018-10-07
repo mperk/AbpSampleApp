de2ead8f9c26a01153c1e3f54d04f3f3a22e5244
Mehmet Perk <perkmehmed@gmail.com>
2018-10-07 11:14:21 +0300

    Add test method

--- test/SampleApp.Application.Tests/SampleAppTestDataBuilder.cs
+++ test/SampleApp.Application.Tests/SampleAppTestDataBuilder.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using SampleApp.Books;
+using System;
+using System.Threading.Tasks;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Identity;
 using Volo.Abp.Threading;
 
@@ -8,10 +11,14 @@ namespace SampleApp
     public class SampleAppTestDataBuilder : ITransientDependency
     {
         private readonly IIdentityDataSeeder _identityDataSeeder;
+        private readonly IRepository<Book, Guid> _bookRepository;
 
-        public SampleAppTestDataBuilder(IIdentityDataSeeder identityDataSeeder)
+        public SampleAppTestDataBuilder(
+            IIdentityDataSeeder identityDataSeeder,
+            IRepository<Book, Guid> bookRepository)
         {
             _identityDataSeeder = identityDataSeeder;
+            _bookRepository = bookRepository;
         }
 
         public void Build()
@@ -22,6 +29,27 @@ namespace SampleApp
         public async Task BuildInternalAsync()
         {
             await _identityDataSeeder.SeedAsync("1q2w3E*");
+
+            await _bookRepository.InsertAsync(
+                new Book
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Test Book 1",
+                    Type = BookType.Dystopia,
+                    PublishDate = new DateTime(1989, 05, 26),
+                    Price = 29
+                }
+                );
+
+            await _bookRepository.InsertAsync(
+                new Book
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Test Book 2",
+                    Type = BookType.Horror,
+                    PublishDate = new DateTime(2016, 09, 07),
+                    Price = 11
+                });
         }
     }
 }
---
+++ test/SampleApp.Application.Tests/Service_Tests/BookAppService_Tests.cs
@@ -0,0 +1,70 @@
+﻿using SampleApp.Services;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Validation;
+using Xunit;
+
+namespace SampleApp.Service_Tests
+{
+    public class BookAppService_Tests : SampleAppApplicationTestBase
+    {
+        private readonly IBookAppService _bookAppService;
+
+        public BookAppService_Tests()
+        {
+            _bookAppService = GetRequiredService<IBookAppService>();
+        }
+
+        [Fact]
+        public async Task Should_Get_List_Of_Books()
+        {
+            var result = await _bookAppService.GetListAsync(
+                    new PagedAndSortedResultRequestDto()
+                    );
+
+            result.TotalCount.ShouldBeGreaterThan(0);
+            result.Items.ShouldContain(x => x.Name == "Test Book 1");
+        }
+
+        [Fact]
+        public async Task Should_Create_A_Valid_Book()
+        {
+            var result = await _bookAppService.CreateAsync(
+                new Books.CreateUpdateBookDto
+                {
+                    Name = "New Test Book 44",
+                    Price = 44,
+                    PublishDate = DateTime.Now,
+                    Type = Books.BookType.ScienceFiction
+                }
+                );
+
+            result.Id.ShouldNotBe(Guid.Empty);
+            result.Name.ShouldBe("New Test Book 44");
+        }
+
+        [Fact]
+        public async Task Should_Not_Craete_A_Book_Without_Name()
+        {
+            var exception = await Assert.ThrowsAsync<AbpValidationException>(async () =>
+            {
+                await _bookAppService.CreateAsync(
+                    new Books.CreateUpdateBookDto
+                    {
+                        Name = "",
+                        Price = 1,
+                        PublishDate = DateTime.Now,
+                        Type = Books.BookType.ScienceFiction
+                    }
+                    );
+            });
+
+            exception.ValidationErrors.ShouldContain(x => x.MemberNames.Any(y => y == "Name"));
+        }
+    }
+}
